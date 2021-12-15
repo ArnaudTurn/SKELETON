b3e5e82c8ded84aefd526b6821a355e5e773351c
ArnaudTurn <35238149+ArnaudTurn@users.noreply.github.com>
2021-12-15 20:12:24 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 setuptools.setup(
     name="Sklon",
     version="0.0.1",
-    author="Arnaud tauveron",
+    author="Arnaud Tauveron",
     author_email="https://www.linkedin.com/in/arnaud-tauveron/",
     description="The skeleton for *Your* data science project",
     long_description=long_description,
